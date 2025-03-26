5f4d05960be49fbe03d76e40f0dca32ed4f3453d
Adrian B <adrianny90@wp.pl>
2025-03-26 11:52:47 +0100

    add new entry

---
+++ src/modules/store.js
@@ -0,0 +1,16 @@
+const storeDiary = (formData) => {
+  const retrieveDiary = JSON.parse(localStorage.getItem("diaryItems")) || [];
+  const checkDate = formData.formData.date;
+  let checkFlag = false;
+  retrieveDiary.forEach((item) => {
+    if (item.formData.date === checkDate) checkFlag = true; //checking if post with such date already axists
+  });
+  if (!checkFlag) {
+    const updatedDiary = [...retrieveDiary, formData];
+    localStorage.setItem("diaryItems", JSON.stringify(updatedDiary));
+    return "";
+  } else {
+    return `Post with such date ${checkDate} already exists`; //warning popup message
+  }
+};
+export { storeDiary };
